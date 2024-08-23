c7fcefec8c47936517f86524c9e83b7bbf58e7c2
erwineka.es <71421531+erwineka123@users.noreply.github.com>
2024-08-23 08:50:54 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return (
       <footer className="bg-white shadow-inner p-4 mt-8">
         <div className="container mx-auto text-center">
-          <p>© 2024 My Portfolio. Pivacy Policy.</p>
+          <p>© 2024 My Portfolio. Privacy Policy.</p>
         </div>
       </footer>
     );
